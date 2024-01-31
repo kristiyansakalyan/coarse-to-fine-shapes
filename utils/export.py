7ccd2d15574be3389355960091cf78219b508f9b
Stephan Schmiedmayer <56692976+StephanSchmiedmayer@users.noreply.github.com>
2024-01-31 12:02:53 +0100

    add file name to export functions

--- utils/export.py
+++ utils/export.py
@@ -1,3 +1,16 @@
+def add_extension_if_missing(filename, extension):
+    """
+    Adds an extension to a filename if it is missing.
+
+    :param filename: Name of the file.
+    :param extension: Extension to add to the filename.
+    :return: Filename with the extension added.
+    """
+    if not filename.endswith(extension):
+        filename += extension
+    return filename
+
+
 def export_pointcloud_to_pts(tensor, filename):
     """
     Exports a PyTorch point cloud tensor to a PTS file.
@@ -5,6 +18,7 @@ def export_pointcloud_to_pts(tensor, filename):
     :param tensor: PyTorch tensor of shape (N, 3) representing the point cloud.
     :param filename: Name of the file to save the point cloud to.
     """
+    filename = add_extension_if_missing(filename, ".pts")
     # Ensure the tensor is on CPU and convert to numpy
     numpy_pc = tensor.cpu().numpy()
 
@@ -23,6 +37,7 @@ def export_pointcloud_to_asc(tensor, filename):
     :param tensor: PyTorch tensor representing the point cloud. Shape should be (N, 3) or (N, 6).
     :param filename: Name of the file to save the point cloud to.
     """
+    filename = add_extension_if_missing(filename, ".asc")
     # Ensure the tensor is on CPU and convert to numpy
     numpy_pc = tensor.cpu().numpy()
 
